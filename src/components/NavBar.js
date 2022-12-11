817c034d3ac42a238a476235fc99dd85b51a3c1f
fusion407 <fusion407@gmail.com>
2022-12-11 16:38:39 -0600

    finished cleaning up Users component and added comments

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-import { BrowserRouter, Route, NavLink, Switch } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 const linkStyles = {
     display: "inline-flex",
     flexdirection: "row",
--- src/components/Users.js
+++ src/components/Users.js
@@ -4,24 +4,26 @@ import User from "./User"
 
 function Users({isLoggedIn, userData}) {
 
-const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([]);
 
-useEffect(() => {
-  setUsers(userData)
-}, [])
-if(!users) return "Loading..."
+  // On page load, set 'users' state to 'userData's username and avatarURL
+  useEffect(() => {
+    setUsers(userData)
+  } , [])
+  if(!users) return "Loading..."
 
-if(!isLoggedIn) return <Redirect to="/login" />
+  // Checks to see if user is logged in
+  if(!isLoggedIn) return <Redirect to="/login" />
 
-const usersToDisplay = users.map((user) => 
-<ul>
-  <User 
-    key={user.id}
-    name={user.username}
-    avatar={user.avatarURL}
-  />
-</ul>
-)
+  const usersToDisplay = users.map((user) => 
+  <ul>
+    <User 
+      key={user.id}
+      name={user.username}
+      avatar={user.avatarURL}
+   />
+  </ul>
+  )
     return (
         <div className="usersBox">
           {users ? usersToDisplay : "No users"}
